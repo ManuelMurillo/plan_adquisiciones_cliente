8791a5dae29030eb8e40701b157893150b3f9536
Jhoan Manuel Murillo Yara <jmmurilloy@correo.udistrital.edu.co>
2020-12-02 15:23:42 -0500

    feat: creacion componente presentacional

--- src/app/pages/peliculas/peliculas.module.ts
+++ src/app/pages/peliculas/peliculas.module.ts
@@ -5,10 +5,11 @@ import { PeliculasRoutingModule } from './peliculas-routing.module';
 import { FormPeliculaComponent } from './components/form-pelicula/form-pelicula.component';
 import { PeliculasComponent } from './components/peliculas/peliculas.component';
 import { DetallePeliculaComponent } from './components/detalle-pelicula/detalle-pelicula.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 
 @NgModule({
-  declarations: [FormPeliculaComponent, PeliculasComponent, DetallePeliculaComponent],
+  declarations: [FormPeliculaComponent, PeliculasComponent, DetallePeliculaComponent, LayoutComponent],
   imports: [
     CommonModule,
     PeliculasRoutingModule
