a84837c61ba9ba564127543cee61c3498305d923
Jhoan Manuel Murillo Yara <jmmurilloy@correo.udistrital.edu.co>
2020-12-02 15:49:34 -0500

    feat: creacion de endpoints de peliculas

--- src/app/pages/peliculas/services/peliculas.service.ts
+++ src/app/pages/peliculas/services/peliculas.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,19 @@ import { Injectable } from '@angular/core';
 })
 export class PeliculasService {
 
-  constructor() { }
+  constructor(private httpService: HttpClient) { }
+
+  getPeliculas() {
+    return this.httpService.get('http://localhost:3000/peliculas')
+  }
+  getPelicula(id: any) {
+    return this.httpService.get('http://localhost:3000/peliculas/' + id)
+  }
+  postPelicula(id: any, data: any) {
+    return this.httpService.post('http://localhost:3000/peliculas/' + id, data)
+  }
+  putPelicula(id: any, data: any) {
+    return this.httpService.put('http://localhost:3000/peliculas/' + id, data)
+  }
+  
 }
