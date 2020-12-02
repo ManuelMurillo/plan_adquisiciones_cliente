a4af5ac5acb7342f31d8c586a037948b2161c52b
Jhoan Manuel Murillo Yara <jmmurilloy@correo.udistrital.edu.co>
2020-12-02 15:56:08 -0500

    feat: ajuste endpoint clasicicaciones

--- src/app/shared/services/peliculas.service.ts
+++ src/app/shared/services/peliculas.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class PeliculasService {
 
-  constructor() { }
+  constructor(private httpService: HttpClient) { }
+
+  getClasificaciones() {
+    return this.httpService.get('http://localhost:3000/clasificaciones');
+  }
 }
